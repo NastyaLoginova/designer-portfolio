0e5dfc4cd9c5cd3b6f289dcfb721b0d97820aaa0
NastyaLoginova <loginovaanasatasiia@gmail.com>
2026-05-22 01:28:58 +0300

    Абсолютный путь

--- shared/links-config.js
+++ shared/links-config.js
@@ -1,5 +1,5 @@
 window.PORTFOLIO_LINKS = {
-  cv: 'assets/cv.pdf',
+  cv: '/designer-portfolio/assets/cv.pdf',
   tg: 'https://t.me/NastyaLoginova',
   linkedin: 'https://www.linkedin.com/in/anastasiia-loginova-881b1b3ba/?locale=ru-RU',
 };
